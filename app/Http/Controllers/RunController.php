8df3513f4681c126a477695b55c3b844a2e7f5cd
natemartin519 <natemartin519@gmail.com>
2015-03-28 18:56:35 -0400

    Added RunController scaffolding

---
+++ app/Http/Controllers/RunController.php
@@ -0,0 +1,59 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Repositories\Interfaces\RunInterface;
+use Illuminate\Http\Request;
+
+class RunController extends Controller {
+
+    protected $runs;
+
+    function __construct(RunInterface $runs)
+    {
+        $this->runs = $runs;
+    }
+
+    /**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+}
---
+++ tests/Controllers/RunControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class RunControllerTest extends TestCase {
+
+    public function testIndexShouldReturnAllRunsInJSONFormat()
+    {
+        $this->fail();
+    }
+
+    public function testShowShouldReturnSingleRunByIdInJSONFormat()
+    {
+        $this->fail();
+    }
+
+    public function testCreateShouldSaveNewRunAndReturn201()
+    {
+        $this->fail();
+    }
+
+    public function testDeleteShouldDeleteRunAndReturn200()
+    {
+        $this->fail();
+    }
+}
